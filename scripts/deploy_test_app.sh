dbde233f3a9d39517db0f9dbb536b190e39e25bd
Nic Cheneweth <nchenewe@thoughtworks.com>
2022-09-26 20:43:12 -0500

    [nc] add signing (testing)
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/deploy_test_app.sh
+++ scripts/deploy_test_app.sh
@@ -24,6 +24,7 @@ spec:
       labels:
         app: tls-test-app
     spec:
+      serviceAccountName: tls-test-app
       initContainers:
         - name: certificate-init-container
           image: twdps/certificate-init-container:dev.${CIRCLE_SHA1:0:8}
